ce619bd3d386837472f5dfd856de2dbb81055b2f
LUCIANO ARAUJO MENESES - GETIC <214083@E077925.int.cagece.com.br>
2020-01-31 15:15:36 -0300

    Deletando arquivo de teste desnecessário

--- helloJest.js
+++
@@ -1,6 +0,0 @@
-function sayTDD(){
-    //criamos a função com exatamente o retorno que foi descrito nos testes
-    return 'TDD é o Desenvolvimento Orientado por Testes'
-}
-
-module.exports = sayTDD;
